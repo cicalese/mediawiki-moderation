67ae5af4ca8ee0f12c21525faca533db2b97a72d
Edward Chernenko <edwardspec@gmail.com>
2021-02-10 23:22:45 +0300

    (troubleshoot)

--- tests/travis/fastest_init_thread.sh
+++ tests/travis/fastest_init_thread.sh
@@ -4,8 +4,7 @@
 # (for parallel testing via Fastest)
 ###############################################################################
 # The following environment variables are provided by Travis:
-# $DBNAME - typically "testwiki"
-# $DBUSER - typically "root"
+# $DBNAME - typically "traviswiki"
 # $DBTYPE - either "mysql" or "postgres"
 ###############################################################################
 
@@ -16,8 +15,8 @@ CLONED_DB_NAME="${ORIGINAL_DB_NAME}_thread${ENV_TEST_CHANNEL}"
 
 # Clone the database (including the initial data, if any).
 if [ "$DBTYPE" = "mysql" ]; then
-	mysql -h 127.0.0.1 -u "${DBUSER}" -e "CREATE DATABASE ${CLONED_DB_NAME}"
-	mysqldump -h 127.0.0.1 -u "${DBUSER}" "${ORIGINAL_DB_NAME}" | mysql -h 127.0.0.1 -u "${DBUSER}" -D "${CLONED_DB_NAME}"
+	mysql -h 127.0.0.1 -e "CREATE DATABASE ${CLONED_DB_NAME}"
+	mysqldump "${ORIGINAL_DB_NAME}" | mysql -D "${CLONED_DB_NAME}"
 else if [ "$DBTYPE" = "postgres" ]; then
 	echo "CREATE DATABASE ${CLONED_DB_NAME} TEMPLATE ${ORIGINAL_DB_NAME};" | psql -U postgres "${ORIGINAL_DB_NAME}"
 fi; fi
